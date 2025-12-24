7477fc9800f55148a78442a3bdd6334c14c83fab
quarterMoon <quarterMoon@eomsanghyeon-ui-MacBookAir.local>
2025-12-24 11:20:31 +0900

    0011

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -4,6 +4,7 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
+import com.back.global.rsData.RsData;
 import com.back.global.shared.post.dto.PostDto;
 import com.back.global.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,12 @@ public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
         Post savedPost = postRepository.save(post);
 
         eventPublisher.publish(new PostCreatedEvent(new PostDto(savedPost)));
 
-        return savedPost;
+        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), savedPost);
     }
 }
