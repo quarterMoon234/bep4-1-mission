39008b40b0006be45fbe3f1e11340b0bd6f78cef
quarterMoon <quarterMoon@eomsanghyeon-ui-MacBookAir.local>
2025-12-26 11:06:07 +0900

    0022

--- src/main/java/com/back/boundedContext/post/in/PostDataInit.java
+++ src/main/java/com/back/boundedContext/post/in/PostDataInit.java
@@ -1,7 +1,5 @@
-package com.back.global.initData;
+package com.back.boundedContext.post.in;
 
-import com.back.boundedContext.member.app.MemberFacade;
-import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.app.PostFacade;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
@@ -12,43 +10,29 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 @Slf4j
-public class DataInit {
-    private final DataInit self;
-    private final MemberFacade memberService;
+public class PostDataInit {
+    private final PostDataInit self;
     private final PostFacade postService;
 
-    public DataInit(
-            @Lazy DataInit self,
-            MemberFacade memberService,
+    public PostDataInit(
+            @Lazy PostDataInit self,
             PostFacade postService
     ) {
         this.self = self;
-        this.memberService = memberService;
         this.postService = postService;
     }
 
     @Bean
-    public ApplicationRunner baseInitDataRunner() {
+    @Order(2)
+    public ApplicationRunner postDataInitApplicationRunner() {
         return args -> {
-            self.makeBaseMembers();
             self.makeBasePosts();
             self.makeBasePostComments();
         };
-    }
-
-    @Transactional
-    public void makeBaseMembers() {
-        if (memberService.count() > 0) return;
-
-        Member systemMember = memberService.join("system", "1234", "시스템").getData();
-        Member holdingMember = memberService.join("holding", "1234", "홀딩").getData();
-        Member adminMember = memberService.join("admin", "1234", "관리자").getData();
-        Member user1Member = memberService.join("user1", "1234", "유저1").getData();
-        Member user2Member = memberService.join("user2", "1234", "유저2").getData();
-        Member user3Member = memberService.join("user3", "1234", "유저3").getData();
     }
 
     @Transactional
