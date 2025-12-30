b30e95968e5816fdd3626030ea8a75bc33b72008
quarterMoon <quarterMoon@eomsanghyeon-ui-MacBookAir.local>
2025-12-31 08:29:10 +0900

    0037

--- src/main/java/com/back/shared/member/out/MemberApiClient.java
+++ src/main/java/com/back/shared/member/out/MemberApiClient.java
@@ -1,15 +1,21 @@
 package com.back.shared.member.out;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MemberApiClient {
-    private static final RestClient restClient = RestClient.builder()
-            .baseUrl("http://localhost:8080/member/api/v1/")
-            .build();
+    private final RestClient restClient;
 
-    public static String getRandomSecureTip() {
+    public MemberApiClient(@Value("${custom.global.internalBackUrl}") String internalBackUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(internalBackUrl + "/api/v1/members")
+                .build();
+    }
+
+    public String getRandomSecureTip() {
         return restClient.get()
                 .uri("/members/randomSecureTip")
                 .retrieve()
